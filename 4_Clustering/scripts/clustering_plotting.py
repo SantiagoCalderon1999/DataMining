59a895d4322777ab9f276feb5a5cd3db7a8ee07d
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-02-01 20:52:31 +0000

    Fix styling warnings

--- 4_Clustering/scripts/clustering_plotting.py
+++ 4_Clustering/scripts/clustering_plotting.py
@@ -9,6 +9,7 @@ def plot_cluster(labels, colors, x, k, centroids = None):
         x_boolean = labels == i
         x_filtered = x[x_boolean]
         plt.scatter(x_filtered[:, 0], x_filtered[:, 1],  color=colors[i], marker='+')
-        if (centroids):
+        if centroids:
             plt.scatter(centroids[i][0], centroids[i][1],  color='red', marker = 'o')
     plt.plot()
+    
