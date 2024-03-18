644fa9ddc81b7ed81087298206fc6fd33a987bf4
Santiago Calderón <santiago.calderon@quorumsoftware.com>
2024-03-18 10:33:24 +0000

    Add third and second coursework exercise

--- Coursework/template_files/adult.py
+++ Coursework/template_files/adult.py
@@ -1,63 +1,69 @@
-
+import pandas as pd
+from sklearn import tree
 # Part 1: Decision Trees with Categorical Attributes
 
 # Return a pandas dataframe with data set to be mined.
 # data_file will be populated with a string 
 # corresponding to a path to the adult.csv file.
 def read_csv_1(data_file):
-	pass
+	return pd.read_csv(data_file)
 
 # Return the number of rows in the pandas dataframe df.
 def num_rows(df):
-	pass
+	return len(df.index)
 
 # Return a list with the column names in the pandas dataframe df.
 def column_names(df):
-	pass
+	return df.columns.tolist()
 
 # Return the number of missing values in the pandas dataframe df.
 def missing_values(df):
-	pass
+	return df.isnull().sum().sum() # Look for a better approach, I don't like this sum sum thing.
 
 # Return a list with the columns names containing at least one missing value in the pandas dataframe df.
 def columns_with_missing_values(df):
-	pass
+	series = df.isnull().sum()
+	return series[series>0].keys().to_list()
 
 # Return the percentage of instances corresponding to persons whose education level is 
 # Bachelors or Masters (by rounding to the first decimal digit)
 # in the pandas dataframe df containing the data set in the adult.csv file.
 # For example, if the percentage is 21.547%, then the function should return 21.6.
 def bachelors_masters_percentage(df):
-	pass
+	return (len(df[(df['education'] == 'Masters') | (df['education'] == 'Bachelors')]) / len(df) * 100) # Avoid magic strings
 
 # Return a pandas dataframe (new copy) obtained from the pandas dataframe df 
 # by removing all instances with at least one missing value.
 def data_frame_without_missing_values(df):
-	pass
+	return df.dropna() # Check if I should use copy
 
 # Return a pandas dataframe (new copy) from the pandas dataframe df 
 # by converting the df categorical attributes to numeric using one-hot encoding.
 # The function's output should not contain the target attribute.
 def one_hot_encoding(df):
-	pass
+	encoded_data = pd.get_dummies(df.dropna()) # Should I usde dropna here?
+	return encoded_data.loc[:, (encoded_data.columns != 'class_<=50K') & (encoded_data.columns != 'class_>50K')]
 
 # Return a pandas series (new copy), from the pandas dataframe df, 
 # containing only one column with the labels of the df instances
 # converted to numeric using label encoding. 
 def label_encoding(df):
-	pass
+	encoded_data = pd.get_dummies(df.dropna()) # Should I drop the dropna?
+	return encoded_data.loc[:, (encoded_data.columns == 'class_<=50K')] # Find a cleaner way to do label encoding
 
 # Given a training set X_train containing the input attribute values 
 # and labels y_train for the training instances,
 # build a decision tree and use it to predict labels for X_train. 
 # Return a pandas series with the predicted values. 
 def dt_predict(X_train,y_train):
-	pass
+	decision_tree = tree.DecisionTreeClassifier()
+	decision_tree.fit(X_train, y_train)
+	return pd.Series(decision_tree.predict(X_train)) # Check if there is a cleaner implementation for this
 
 # Given a pandas series y_pred with the predicted labels and a pandas series y_true with the true labels,
 # compute the error rate of the classifier that produced y_pred.  
 def dt_error_rate(y_pred, y_true):
-	pass
-
-
-
+	score = 0
+	for output in zip(y_pred, y_true):
+		score = (score + 1) if output[0] == output[1] else score
+	return score / len(y_true) # Ensure that this works and ensure that this is exactly what he means by error rate
--- Coursework/template_files/wholesale_customers.py
+++ Coursework/template_files/wholesale_customers.py
@@ -1,22 +1,34 @@
+import pandas as pd
+import sklearn.cluster as cluster
+import sklearn.metrics as metrics
+
 # Part 2: Cluster Analysis
 
 # Return a pandas dataframe containing the data set that needs to be extracted from the data_file.
 # data_file will be populated with the string 'wholesale_customers.csv'.
 def read_csv_2(data_file):
-	pass
+	data = pd.read_csv(data_file)
+	data = data.loc[:, (data.columns != 'Channel') & (data.columns != 'Region')]
+	return data
 
 # Return a pandas dataframe with summary statistics of the data.
 # Namely, 'mean', 'std' (standard deviation), 'min', and 'max' for each attribute.
 # These strings index the new dataframe columns. 
 # Each row should correspond to an attribute in the original data and be indexed with the attribute name.
 def summary_statistics(df):
-	pass
+	data_statistics = []
+	for column in df.columns: # Is there a better way than just using the for?
+		data_statistics.append([round(df[column].mean()), round(df[column].std()), df[column].min(), df[column].max()]) # Ensure round works fine
+	return pd.DataFrame(data_statistics, columns=['mean', 'std', 'min', 'max']).set_index(df.columns)
 
 # Given a dataframe df with numeric values, return a dataframe (new copy)
 # where each attribute value is subtracted by the mean and then divided by the
 # standard deviation for that attribute.
 def standardize(df):
-	pass
+	standardized_data = df.copy()
+	for column in df.columns:
+		standardized_data[column] = (standardized_data[column] - df[column].mean()) / df[column].std()
+	return standardized_data
 
 # Given a dataframe df and a number of clusters k, return a pandas series y
 # specifying an assignment of instances to clusters, using kmeans.
@@ -24,24 +36,30 @@ def standardize(df):
 # To see the impact of the random initialization,
 # using only one set of initial centroids in the kmeans run.
 def kmeans(df, k):
-	pass
+	km = cluster.KMeans(n_clusters=k)
+	km.fit(df)
+	return pd.Series(km.labels_)
 
 # Given a dataframe df and a number of clusters k, return a pandas series y
 # specifying an assignment of instances to clusters, using kmeans++.
 # y should contain values from the set {0,1,...,k-1}.
 def kmeans_plus(df, k):
-	pass
+	kmpp = cluster.KMeans(n_clusters=k, init='k-means++')
+	kmpp.fit(df)
+	return pd.Series(kmpp.labels_)
 
 # Given a dataframe df and a number of clusters k, return a pandas series y
 # specifying an assignment of instances to clusters, using agglomerative hierarchical clustering.
 # y should contain values from the set {0,1,...,k-1}.
 def agglomerative(df, k):
-	pass
+	agg = cluster.AgglomerativeClustering(n_clusters = k, linkage='average') # Add euclidean distance
+	agg.fit(df)
+	return pd.Series(agg.labels_)
 
 # Given a data set X and an assignment to clusters y
 # return the Silhouette score of this set of clusters.
 def clustering_score(X,y):
-	pass
+	return metrics.silhouette_score(X, y, metric = 'euclidean')
 
 # Perform the cluster evaluation described in the coursework description.
 # Given the dataframe df with the data to be clustered,
@@ -52,16 +70,69 @@ def clustering_score(X,y):
 # 'k': the number of clusters produced,
 # 'Silhouette Score': for evaluating the resulting set of clusters.
 def cluster_evaluation(df):
-	pass
+    ks = [3, 5, 10]
+    number_iterations=10
+    results = []
+    data_types = ['Original', 'Standardized']
+    for data_type in data_types:
+        if(data_types == 'Original'):
+            df_used = df.copy()
+        else:
+            df_used = standardize(df.copy())
+            
+        for k in ks:
+            SC_cummulative = []
+            for i in range(0, number_iterations):
+                km = cluster.KMeans(n_clusters=k, n_init=10)
+
+                km.fit(df_used)
+
+                SC_cummulative.append(metrics.silhouette_score(df_used, km.labels_, metric = 'euclidean'))
+            SC = max(SC_cummulative)
+            results.append(['Kmeans', data_type, k, SC])
+        for k in ks:
+            #km = cluster.AgglomerativeClustering(n_clusters = k, linkage='average', metric='euclidean')
+            km = cluster.AgglomerativeClustering(n_clusters = k, linkage='average') # See if Euclidean metric can now be used
+
+            km.fit(df_used)
+
+            SC = metrics.silhouette_score(df_used, km.labels_, metric = 'euclidean')
+            
+            results.append(['Agglomerative', data_type, k, SC])
+    return pd.DataFrame(results, columns=['Algorithm', 'data', 'k', 'Silhouette Score'])
 
 # Given the performance evaluation dataframe produced by the cluster_evaluation function,
 # return the best computed Silhouette score.
 def best_clustering_score(rdf):
-	pass
+	return max(rdf['Silhouette Score'])
 
 # Run the Kmeans algorithm with k=3 by using the standardized data set.
 # Generate a scatter plot for each pair of attributes.
 # Data points in different clusters should appear with different colors.
 def scatter_plots(df):
-	pass
+	k = 3
+	km = cluster.KMeans(n_clusters=k)
+	df = standardize(df)
+	km.fit(df)
+	import numpy as np
+	colors = []
+	for i in range(0, k):
+		colors.append(np.random.rand(3,1).flatten())
 
+	for i in range(0, len(df.columns)):
+		label_1 = df.columns[i]
+		for j in range(i+1, len(df.columns)):
+				label_2 = df.columns[j]
+				_plot_cluster(km.labels_, colors, df.loc[:, (df.columns == label_1) | (df.columns == label_2)], k, label_1, label_2)
+
+import matplotlib.pyplot as plt
+def _plot_cluster(labels, colors, x, k,label_1,label_2):
+    """Plots the clusterization result
+    """
+    for i in range(0, k):
+        x_boolean = labels == i
+        x_filtered = x[x_boolean].to_numpy()
+        plt.scatter(x_filtered[:, 0], x_filtered[:, 1],  color=colors[i], marker='+')
+    plt.title(f"{label_1} vs {label_2}")
+    plt.plot()
+    plt.show()
